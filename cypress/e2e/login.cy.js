0cbcb43cece8275618c5f5660f896eb87e9afc13
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2026-01-14 19:01:08 +0300

    second commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,39 +1,45 @@
-it("should open main page", ()=>{
-  cy.visit("http://localhost:3000/");
-  cy.contains("Books list");   
-})
+describe("Тесты на авторизацию сайта библиотека", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Books list");
+  });
 
-it("Should successfully login", () => {
-  cy.visit("/");
-  cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
-});
+  it("Should successfully login", () => {
+    cy.visit("/");
+    cy.login("test@test.com", "test");
+    cy.contains("Добро пожаловать test@test.com").should("be.visible");
+  });
 
-it("success login", function () {
-  cy.visit('/');
-  cy.contains("Log in").click();
-  cy.get("#mail").type("bropet@mail.ru");
-  cy.get("#pass").type("123");
-  cy.contains("Submit").click();
-  cy.contains("bropet@mail.ru").should('be.visible');
-});
+  it("success login", function () {
+    cy.visit("/");
+    cy.contains("Log in").click();
+    cy.get("#mail").type("bropet@mail.ru");
+    cy.get("#pass").type("123");
+    cy.contains("Submit").click();
+    cy.contains("bropet@mail.ru").should("be.visible");
+  });
 
-it("Should not login with empty login", () => {
-  cy.visit("/");
-  cy.contains("Log in").click();
-  cy.get("#mail").type(" ");
-  cy.get("#pass").type("test");
-  cy.contains("Submit").click();
-  cy.get("#mail").then(($el) => $el[0].checkValidity()).should("be.false");
-  cy.get("#mail").then(($el) => $el[0].validationMessage)
-    .should("contain", "Заполните это поле.");
-});
+  it("Should not login with empty login", () => {
+    cy.visit("/");
+    cy.contains("Log in").click();
+    cy.get("#mail").type(" ");
+    cy.get("#pass").type("test");
+    cy.contains("Submit").click();
+    cy.get("#mail")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+    cy.get("#mail")
+      .then(($el) => $el[0].validationMessage)
+      .should("contain", "Заполните это поле.");
+  });
 
-it("Should not login with empty password", () => {
-  cy.visit("/booksNode");
-  cy.contains("Log in").click();
-  cy.get("#mail").type("test@test.com");
-  cy.contains("Submit").click();
-  cy.get("#pass").then(($el) => $el[0].checkValidity())
-    .should("be.false");
+  it("Should not login with empty password", () => {
+    cy.visit("/booksNode");
+    cy.contains("Log in").click();
+    cy.get("#mail").type("test@test.com");
+    cy.contains("Submit").click();
+    cy.get("#pass")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+  });
 });
